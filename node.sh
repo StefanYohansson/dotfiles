47d9e4b64591bdf5ae64eb042f1a6d1d354a5492
StefanYohansson <sy.fen0@gmail.com>
2016-12-08 16:36:49 -0300

    Install node latest script.

---
+++ node.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install stable
