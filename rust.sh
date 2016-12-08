cd06b88b238439ad763d74bb40b6ae83ca04604b
StefanYohansson <sy.fen0@gmail.com>
2016-12-08 16:30:29 -0300

    Added racer to rust installation.

--- rust.sh
+++ rust.sh
@@ -2,4 +2,4 @@
 
 [[ -s /home/snotr/.rsvm/rsvm.sh ]] && . /home/snotr/.rsvm/rsvm.sh
 curl -L https://raw.github.com/sdepold/rsvm/master/install.sh | sh
-rsvm install nightly
+rsvm install nightly && cargo install racer
