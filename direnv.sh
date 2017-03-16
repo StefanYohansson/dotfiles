f1e5aefe7929da3aabded17932079ae53e5893d9
StefanYohansson <sy.fen0@gmail.com>
2017-03-16 10:38:53 -0300

    Update direnv installation.

--- direnv.sh
+++ direnv.sh
@@ -3,6 +3,6 @@
 # get latest version of direnv
 # you can use apt or pacman
 # but pacman version is currently outdated
-curl -o /opt/direnv $(curl -s https://api.github.com/repos/direnv/direnv/releases/latest | jq --raw-output '.assets | map(.browser_download_url) | map(select(. | test(".*direnv.linux-amd64.*"))) | .[0]')
+wget -O /opt/direnv $(curl -s https://api.github.com/repos/direnv/direnv/releases/latest | jq --raw-output '.assets | map(.browser_download_url) | map(select(. | test(".*direnv.linux-amd64.*"))) | .[0]')
 chmod +x /opt/direnv
 ln -s /opt/direnv /usr/bin/
